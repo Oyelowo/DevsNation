da7bd205807178e5c46c19cb0bb5af10d46d64ac
Oyelowo Oyedayo <oyelowo.oyedayo@relex.fi>
2019-01-04 22:54:50 +0200

    app listening

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const www = process.env.WWW || './';
+const www = process.env.WWW || "./";
 
 app.use(express.static(www));
 
 console.log(`serving ${www}`);
 
-app.get('/', (req, res)=>res.send('Hellrero'));
+app.get("/", (req, res) => res.send("Hellrero"));
+
+app.listen(port, (req, res) => {
+  console.log(`listening on port${port}`);
+});
