52c1a974860c385e8a928a6635ff598987281f84
Oyelowo Oyedayo <oyelowo.oyedayo@relex.fi>
2019-01-05 17:09:27 +0200

    initial express server with route files

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const app = express();
 
 // DB Config
 //const db = require("./config/keys").mongoURI;
-
 const { mongoURI: db } = require("./config/keys");
 
 // Connect to MongoDB
-
 mongoose.connect(db).then(()=> console.log("mongoDB connected")).catch(err => console.log('err'));
 
-const port = process.env.PORT || 5000;
+
 
 app.get("/", (req, res) => res.send("Hellokekk"));
 
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`listening on http://localhost:${port}`));
