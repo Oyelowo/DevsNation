35e8b8a6ef2dd30731f661c7aafd7efcb5b41fc2
Oyelowo Oyedayo <oyelowo.oyedayo@relex.fi>
2019-01-13 07:07:09 +0200

    Redux Actions and Proptypes

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,13 +1,18 @@
+import { TEST_DISPATCH } from "../actions/types";
+
 const initialState = {
     isAuthenticated: false,
     user: {}
 }
-const SET_CURRENT_USER = null;
+
 
 export default (state = initialState, action) => {
     switch (action.type) {
-        case SET_CURRENT_USER:
-            return ''
+        case TEST_DISPATCH:
+            return {
+                ...state,
+                user: action.payload
+            }
 
         default:
             return state;
